cab845340b6ca60f3385515b648ec4913cc5200f
Koffi Sani <koffisani@gmail.com>
2017-10-24 14:39:26 +0000

    Initial commit

---
+++ src/main/java/com/example/demo/TpSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringMvcApplication.class, args);
+	}
+}
